d7807820856f8f097a390d333c124035d069a3f0
verybada <lon_bad@hotmail.com>
2015-09-06 18:08:55 +0800

    Add comment

--- algorithm/findMedianSortedArrays/findMedianSortedArrays.py
+++ algorithm/findMedianSortedArrays/findMedianSortedArrays.py
@@ -12,15 +12,18 @@ class Solution(object):
         >>> obj.findMedianSortedArrays([], [1,2,3,4,5])
         3.0
         """
+        # Merge two lists and sort them
         nums1.extend(nums2)
         new_list = sorted(nums1)
 
         devider = 1.0
         listlen = len(new_list)
+        # find the middle position of this list
         middle = listlen >> 1
-        remain = listlen % 2
+        remain = listlen % 2 # check list is odd or not
         median = new_list[middle-1+remain]
         if remain == 0:
+            # Needs to calculate median with 2 numbers when list is not odd
             median += new_list[middle]
             devider = 2.0
 
