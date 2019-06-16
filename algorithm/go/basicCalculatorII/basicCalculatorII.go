6cf40c37b1b783e4ef77242258a19dd32d521f9e
verybada <lon_bad@hotmail.com>
2019-06-16 19:42:14 +0800

    go: more efficiently solution for basicCalculatorII

--- algorithm/go/basicCalculatorII/basicCalculatorII.go
+++ algorithm/go/basicCalculatorII/basicCalculatorII.go
@@ -1,97 +1,53 @@
 package basicCalculatorII
 
 import (
-    "strings"
-    "strconv"
 )
 
-type stack struct{
-    s []int
-}
-
-func (s *stack) sum() int {
-    num := 0
-    for _, value := range s.s {
-        num += value
-    }
-    return num
-}
-
-func (s *stack) push(value int) {
-    s.s = append(s.s, value)
-}
-
-func (s *stack) pop() int {
-    l := len(s.s)
-    v := s.s[l-1]
-    s.s = s.s[:l-1]
-    return v
-}
-
-func isSymbo(s string) bool {
-    switch(s) {
-    case "+":
-        fallthrough
-    case "-":
-        fallthrough
-    case "*":
-        fallthrough
-    case "/":
-        return true
-    default:
-        return false
-    }
-}
-
 func calculate(s string) int {
-    // Trim all space
-    s = strings.Replace(s, " ", "", -1)
-
-    numS := stack{
-        make([]int, 0, len(s)),
-    }
-    getNum := func(num int, symbo string) int {
-        switch(symbo) {
-        case "+":
-            return num
-        case "-":
-            return -num
-        case "*":
-            prev_num := numS.pop()
-            return prev_num*num
-        case "/":
-            prev_num := numS.pop()
-            return prev_num/num
-        }
-
-        return -1
-    }
-
-    chars := strings.Split(s, "")
-    prev_i := 0
-    prev_symbo := "+"
-    for i := 0; i<len(chars); i++ {
-        c := chars[i]
-
-        switch(c) {
-        case "+", "-", "*", "/":
-            num, err := strconv.Atoi(s[prev_i: i])
-            if err != nil {
-                panic(err)
+    numS := make([]int, 0, len(s))
+    num := 0
+    prev_sign := '+'
+    for _, c := range s {
+        if c == ' ' {
+            continue
+        } else if c >= '0' && c <= '9' {
+            num = num*10 + int(c - '0')
+        } else {
+            if prev_sign == '+' {
+                numS = append(numS, num)
+            } else if prev_sign == '-' {
+                numS = append(numS, -num)
+            } else if prev_sign == '*' {
+                last_index := len(numS)-1
+                prev_num := numS[last_index]
+                numS[last_index] = prev_num*num
+            } else if prev_sign == '/' {
+                last_index := len(numS)-1
+                prev_num := numS[last_index]
+                numS[last_index] = prev_num/num
             }
-
-            num = getNum(num, prev_symbo)
-            numS.push(num)
-            prev_symbo = c
-            prev_i = i+1
-            break
+            num = 0
+            prev_sign = c
         }
     }
-    num, err := strconv.Atoi(s[prev_i:])
-    if err != nil {
-        panic(err)
+
+    if prev_sign == '+' {
+        numS = append(numS, num)
+    } else if prev_sign == '-' {
+        numS = append(numS, -num)
+    } else if prev_sign == '*' {
+        last_index := len(numS)-1
+        prev_num := numS[last_index]
+        numS[last_index] = prev_num*num
+    } else if prev_sign == '/' {
+        last_index := len(numS)-1
+        prev_num := numS[last_index]
+        numS[last_index] = prev_num/num
     }
-    num = getNum(num, prev_symbo)
-    numS.push(num)
-    return numS.sum()
+
+    sum := 0
+    for _, num := range numS {
+        sum += num
+    }
+    return sum
 }
